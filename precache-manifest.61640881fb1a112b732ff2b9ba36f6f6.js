1b4d9139407f79da8e7851ed3595dcaafab5dcf8
Ahmed Maqbool <39122332+AhmedMaqbool@users.noreply.github.com>
2020-08-08 14:10:55 +0300

    Updates

---
+++ precache-manifest.61640881fb1a112b732ff2b9ba36f6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a198c65b14c8a4c11e5f5c92e12911f1",
+    "url": "/Currency-Convertor/index.html"
+  },
+  {
+    "revision": "b8bf9e3caa752282bb48",
+    "url": "/Currency-Convertor/static/css/main.384458f6.chunk.css"
+  },
+  {
+    "revision": "1bfea218baf9466ee627",
+    "url": "/Currency-Convertor/static/js/2.d4c83202.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency-Convertor/static/js/2.d4c83202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bf9e3caa752282bb48",
+    "url": "/Currency-Convertor/static/js/main.547cf8a1.chunk.js"
+  },
+  {
+    "revision": "2c13c8f547cf35f71bf9",
+    "url": "/Currency-Convertor/static/js/runtime-main.65000c8d.js"
+  }
+]);
